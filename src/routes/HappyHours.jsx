631050b673342a65c7cb23bd3dea867378b5b314
george <gelkik@gmail.com>
2023-08-23 15:32:58 -0400

    Added first batch of components

---
+++ src/routes/HappyHours.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HappyHours = () => {
+    return (
+        <h1>HappyHours</h1>
+    )
+}
+
+export default HappyHours;
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+			primary: "#F1F1F0",
+			secondary: "#E68383",
+			tertiary: "#444054",
+		},
+		extend: {
+			fontFamily: {
+				main: ["Quicksand", "sans-serif"],
+			},
+			screens: {
+				maxPro: "430px",
+				iphoneXr: "390px",
+				samsungS8: '360px',
+			},
+			width: {
+				81: "81.396%",
+				69: "69.428%",
+			},
+			padding: {
+				18: "40%",
+			},
+
+			spacing: {
+				22: "88px",
+				193: "192.79px",
+			},
+			fontSize: {
+				"7xl": "56px",
+			},
+		},
+
+  },
+  plugins: [],
+}
+
