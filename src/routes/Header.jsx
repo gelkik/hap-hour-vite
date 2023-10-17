f66d046051412abafe08180a9e014b970dd43e54
george <gelkik@gmail.com>
2023-10-17 19:14:14 -0400

    Added navbar

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 
 const Header = () => {
     return (
         <>
-            <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <nav class="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
+                <div class="flex w-full flex-wrap items-center justify-between px-3">
+                    <a class="ml-2 text-xl text-neutral-800 dark:text-neutral-200"
+                    href="#"><Link to="/">Home</Link></a>
+                    <div class="ml-5 flex w-[30%] items-center justify-between">
+                    <input
+                        type="search"
+                        class="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none motion-reduce:transition-none dark:border-neutral-500 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="button-addon2" />
+                    <span
+                        class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+                        id="basic-addon2">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        class="h-5 w-5">
+                        <path
+                            fill-rule="evenodd"
+                            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                            clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    </div>
+                    <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                        <div class="py-1" role="none">
+                        {user ? <Link to="/happyhours">Happy Hours</Link> : <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">
+                            <Link to="/signin">Sign In</Link>
+                        </a>}
+                        <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
+                        <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                        <form method="POST" action="#" role="none">
+                            <button type="submit" class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+ {/* <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
                 <div class="flex items-center flex-shrink-0 text-white mr-6">
                     <span class="font-semibold text-xl tracking-tight">Happy Hour NYC</span>
                 </div>
@@ -27,16 +71,13 @@ const Header = () => {
                     </a>
                     </div>
                 </div>
-            </nav>
-        </>
-    )
-    //  <div className="navbar">
+            </nav> */}
+    {/* //  <div className="navbar">
     //     <ul className="nav-links">
     //         <Link to="/">Home</Link>
     //         <Link to="/happyhours">Happy Hours</Link>
     //         <Link to="/signin">Sign In</Link>
     //     </ul>
-    // </div>
-}
+    // </div> */}
 
 export default Header;
