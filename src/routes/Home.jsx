d4bc1ec938237d8cf3e9b986539c0bc0113c2667
george <gelkik@gmail.com>
2023-12-05 22:04:28 -0500

    Making the happy hour cards through the seed data

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import HappyHour from "./HappyHour";
+import seedData from '../seedData.json'
 
 const Home = () => {
+    
+    // console.log(seedData[0].restaurant_name)
     return (
-        <HappyHour />
+        // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div>
+            {seedData.map((seed) => (
+                <HappyHour
+                    key={seed.restaurant_name} 
+                    name={seed.restaurant_name}
+                    time={seed.happy_hour_time}
+                    link={seed.link}
+                    food={seed.food_offered}
+                />
+            ))}
+        </div>
+        
     )
 }
 
