84785e050d2cfb1504cb1769c17a3476c89e12e8
george <gelkik@gmail.com>
2023-12-10 19:21:16 -0500

    Trying to add maps api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import HappyHour from "./HappyHour";
 import seedData from '../seedData.json'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const Home = () => {
+    const maps_key = import.meta.env.GOOGLE_MAPS_API_KEY
     
-    // console.log(seedData[0].restaurant_name)
+    const [map,setMap] = useState(null)
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: `${maps_key}`
+      })
+    
+    const containerStyle = {
+        width: '400px',
+        height: '400px',
+    };
+
+    const center = {
+        lat: -34.397,
+        lng: 150.644,
+    };
+
+    const handleMapLoad = (map) => {
+        setMap(map);
+    };
+
     return (
         // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <div>
+            {isLoaded && (
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={center}
+                    zoom={10}
+                    onLoad={handleMapLoad}
+                >
+                    {/* {seedData.map((seed) => (
+                        <Marker
+                            key={seed.restaurant_name}
+                            position={{ lat: seed.latitude, lng: seed.longitude }}
+                        />
+                    ))} */}
+                </GoogleMap>
+            )}
+
             {seedData.map((seed) => (
                 <HappyHour
                     key={seed.restaurant_name} 
